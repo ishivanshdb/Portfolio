8f2b471ac78934e1d6b747f58f602ac5bc1c076c
Shivansh Dubey <37132580+shivanshd@users.noreply.github.com>
2020-11-26 01:07:07 +0530

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+  $('.slider').slick({
+    arrows: false,
+    dots: true,
+    appendDots: '.slider-dots',
+    dotsClass: 'dots'
+  })
+})
+
+$(document).ready(function () {
+  $('.slider-2').slick({
+    arrows: false,
+    dots: true,
+    appendDots: '.slider-2-dots',
+    dotsClass: 'dots'
+  })
+})
+
+
